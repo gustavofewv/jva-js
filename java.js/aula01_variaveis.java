46b69964f1a92f401f6c3eaa4887abb445b5a13a
gustavofewv <gustavo.oliveira.alcantara@escola.pr.gov.br>
2026-06-24 09:54:20 -0300

    Add files via upload

---
+++ java.js/aula01_variaveis.java
@@ -0,0 +1,18 @@
+package java.js;
+
+public class aula01_variaveis{
+
+    public  static void main(string[] args){
+    int idade = 17;
+    double altura = 1.75;
+    boolean  matriculado = true;
+    string nome = "murilo";
+    
+    
+    systen.out.println("nome" + nome);
+      systen.out.println("idade" + idade + "anos");
+        systen.out.println("altura" + altura + "m");
+          systen.out.println("nome matriculado?" + matriculado);
+
+}
+}
